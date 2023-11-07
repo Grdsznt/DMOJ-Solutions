6e37934b0af30a09e8437db92442dc0d72d280eb
Grdsznt <68165351+Grdsznt@users.noreply.github.com>
2023-11-07 08:26:16 -0500

    Update firstInteractiveproblem.cpp

--- firstInteractiveproblem.cpp
+++ firstInteractiveproblem.cpp
@@ -2,35 +2,35 @@
 using namespace std;
 typedef long long ll;
 string s;
-// int main() {
-//     ll low = 1, high = (ll) 2e9;
-//     while (low <= high) {
-//         ll middle = floor((low + high)/2);
-//         cout << middle << endl;  cin >> s;
-//         if (s == "OK") {return 0;}
-//         else if(s == "FLOATS") {high = middle-1;}
-//         else {low = middle + 1;}
-//     }
+int main() {
+    ll low = 1, high = (ll) 2e9;
+    while (low <= high) {
+        ll middle = floor((low + high)/2);
+        cout << middle << endl;  cin >> s;
+        if (s == "OK") {return 0;}
+        else if(s == "FLOATS") {high = middle-1;}
+        else {low = middle + 1;}
+    }
     
 
 
+}
+
+// void generate_permutations(string s) {
+//     int n = s.length();
+//     int count = 0;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i; j < n; j++) {
+//             string temp = s;
+//             char c = temp[i];
+//             temp[i] = temp[j];
+//             temp[j] = c;
+//             count++;
+//             cout << temp << endl;
+//         }
+//     }        
 // }
 
-void generate_permutations(string s) {
-    int n = s.length();
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            string temp = s;
-            char c = temp[i];
-            temp[i] = temp[j];
-            temp[j] = c;
-            count++;
-            cout << temp << endl;
-        }
-    }        
-}
-
-int main() {
-    generate_permutations("aa");
-}
+// int main() {
+//     generate_permutations("aa");
+// }
